acc8c4be2e0dc939069326046d05341f05768dcf
Alexandre Alunno <alexandrealunno@gmail.com>
2021-10-07 15:06:48 +0200

    modified Scrap one page file + created recuperation_url file
    -Created function scrap_data_books

--- Scrap_one_page.py
+++ Scrap_one_page.py
@@ -2,51 +2,59 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-reponse = requests.get(url)
-page = reponse.content
-soup = BeautifulSoup(page, "html.parser")
+url = "http://books.toscrape.com/catalogue/sharp-objects_997/index.html"
 
-#récupération categorie
-genre = soup.find_all("a")
-categorie = []
-for type in genre:
-    categorie = [genre[3].string]
-#print(categorie)
+def scrap_data_books(url):
+    reponse = requests.get(url)
+    page = reponse.content
+    soup = BeautifulSoup(page, "html.parser")
 
-
-#récupération du titre
-titre = soup.find_all("h1")
-titre_officiel = []
-for title in titre:
-    titre_officiel.append(title.string)
-#print(titre_officiel)
+    #récupération categorie
+    genre = soup.find_all("a")
+    categorie = []
+    for type in genre:
+        categorie = [genre[3].string]
+    #print(categorie)
 
 
-#récupération de la description
-description = soup.find_all("p")
-product_description = []
-for descri in description:
-    product_description = [description[3].string]
-#print(product_description)
+    #récupération du titre
+    titre = soup.find_all("h1")
+    titre_officiel = []
+    for title in titre:
+        titre_officiel.append(title.string)
+    #print(titre_officiel)
 
 
-#récupération des données du tableau de fin de page
-tableau = soup.find_all("td")
-data_tableau = []
-for data in tableau:
-    data_tableau.append(data.string)
-#print(data_tableau)
+    #récupération de la description
+    description = soup.find_all("p")
+    product_description = []
+    for descri in description:
+        product_description = [description[3].string]
+    #print(product_description)
 
-#récupération image
-image = soup.find_all("img")
-image_url = image[0].get("src")
-incomplete_url = image_url.split("../..")
-root_url = "http://books.toscrape.com"
-complete_url = [root_url + incomplete_url[1]]
-print(complete_url)
 
-book_data = categorie + titre_officiel + product_description + data_tableau + complete_url
+    #récupération des données du tableau de fin de page
+    tableau = soup.find_all("td")
+    data_tableau = []
+    for data in tableau:
+        data_tableau.append(data.string)
+    #print(data_tableau)
+
+    #récupération image + reconstruction url complète
+    image = soup.find_all("img")
+    image_url = image[0].get("src")
+    incomplete_url = image_url.split("../..")
+    root_url = "http://books.toscrape.com"
+    complete_url = [root_url + incomplete_url[1]]
+    #print(complete_url)
+
+    book_data = categorie + titre_officiel + product_description + data_tableau + complete_url
+
+    return book_data
+
+book_data = scrap_data_books(url)
+
+print(book_data)
 
 en_tete = ["Categorie", "Titre", "Description", "UPC", "Type", "Prix Hors Tax", "Prix Avec Tax", "Tax", "Disponnibilité", "Nombre d'avis", "URL Couverture"]
 with open("data_livre.csv", "w") as csv_file:
