4ad10e00ba7d83b302ef452b6077cdb4a333ea59
Alexandre Alunno <alexandrealunno@gmail.com>
2021-10-07 12:10:43 +0200

    Modified Scrap one page file:
    -Implemented image url scraping
    -Format image url + root url
    -return complete url to the image

--- Scrap_one_page.py
+++ Scrap_one_page.py
@@ -12,8 +12,7 @@ genre = soup.find_all("a")
 categorie = []
 for type in genre:
     categorie = [genre[3].string]
-
-print(categorie)
+#print(categorie)
 
 
 #récupération du titre
@@ -21,26 +20,35 @@ titre = soup.find_all("h1")
 titre_officiel = []
 for title in titre:
     titre_officiel.append(title.string)
-print(titre_officiel)
+#print(titre_officiel)
+
 
 #récupération de la description
 description = soup.find_all("p")
 product_description = []
 for descri in description:
     product_description = [description[3].string]
-print(product_description)
+#print(product_description)
+
 
 #récupération des données du tableau de fin de page
 tableau = soup.find_all("td")
 data_tableau = []
 for data in tableau:
     data_tableau.append(data.string)
+#print(data_tableau)
 
-print(data_tableau)
+#récupération image
+image = soup.find_all("img")
+image_url = image[0].get("src")
+incomplete_url = image_url.split("../..")
+root_url = "http://books.toscrape.com"
+complete_url = [root_url + incomplete_url[1]]
+print(complete_url)
 
-book_data = categorie + titre_officiel + product_description + data_tableau
+book_data = categorie + titre_officiel + product_description + data_tableau + complete_url
 
-en_tete = ["Categorie", "Titre", "Description", "UPC", "Type", "Prix Hors Tax", "Prix Avec Tax", "Tax", "Disponnibilité", "Nombre d'avis"]
+en_tete = ["Categorie", "Titre", "Description", "UPC", "Type", "Prix Hors Tax", "Prix Avec Tax", "Tax", "Disponnibilité", "Nombre d'avis", "URL Couverture"]
 with open("data_livre.csv", "w") as csv_file:
     writer = csv.writer(csv_file, delimiter=",")
     writer.writerow(en_tete)
