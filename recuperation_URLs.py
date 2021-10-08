dce1602ba508d1949c6701645755f60011a6cf09
Alexandre Alunno <alexandrealunno@gmail.com>
2021-10-08 15:15:43 +0200

    Modified URL scraping
    -Created function recuperation_urls_books(url)
    -cleaned file recuperation_URLs

--- recuperation_URLs.py
+++ recuperation_URLs.py
@@ -1,42 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+def recuperation_urls_books(url):
+    reponse = requests.get(url)
+    page = reponse.content
+    soup = BeautifulSoup(page, "html.parser")
+    url_next_book = soup.find_all("div", class_="image_container")
+    list_url_book = []
 
-reponse = requests.get(url)
-page = reponse.content
-soup = BeautifulSoup(page, "html.parser")
-
-"""url_next_book = soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
-incomplete_url = str(url_next_book[0]).split('href="../../..')
-book_url = incomplete_url[1].split('"')
-print(book_url[0])"""
-
-url_next_book = soup.find_all("div", class_="image_container")
-#incomplete_url = url_next_book[0].findChildren("a", recursive=False)
-#image_url = incomplete_url[0].get("href")
-list_url_book = []
-
-for image in url_next_book:
-    incomplete_url = image.findChildren("a", recursive=False)
-    #print(incomplete_url)
-    image_url = incomplete_url[0].get("href")
-    #print("image url: " + str(image_url))
-    short_url = image_url.split("../../..")
-    root_url = "http://books.toscrape.com/catalogue"
-    complete_url = root_url + short_url[0]
-    list_url_book.append(complete_url)
-
-print(list_url_book)
-
-#print(url_next_book)
-#for book in incomplete_url:
-   # print(book)
+    for image in url_next_book:
+        incomplete_url = image.findChildren("a", recursive=False)
+        #print(incomplete_url)
+        image_url = incomplete_url[0].get("href")
+        #print("image url: " + str(image_url))
+        short_url = image_url.split("../../..")
+        root_url = "http://books.toscrape.com/catalogue"
+        complete_url = root_url + short_url[1]
+        list_url_book.append(complete_url)
+    #print(list_url_book)
+    return list_url_book
 
 
-"""next_book = soup.find_all("h3")
-url_next_book = []
-for adress in next_book:
-    url_next_book.append(adress)
-print(next_book)
-print(url_next_book)"""
+if __name__ == '__main__':
+    url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+    reponse = requests.get(url)
+    page = reponse.content
+    soup = BeautifulSoup(page, "html.parser")
+    url_next_book = soup.find_all("div", class_="image_container")
+    list_url_book = []
+    recuperation_urls_books(url)
