6afeb204c4b054c9a22e8f55c8b9e6f26cb3b705
alexandre alunno <alexandrealunno@gmail.com>
2021-10-11 11:56:06 +0200

    Changing page
    -created for loop giving url of each page of a category
    -To do: script to get the category's url and name + script to change root url with category+page's number

--- Changing_page.py
+++ Changing_page.py
@@ -3,15 +3,26 @@ from bs4 import BeautifulSoup
 from math import *
 
 
+#calculate the number of books in a category
 category_url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
 reponse = requests.get(category_url)
 page = reponse.content
 soup = BeautifulSoup(page, "html.parser")
 emplacement = soup.find_all("strong")
 total_number_books = emplacement[1].string
-print(total_number_books)
+print(total_number_books.string)
 
+#calculate the number of pages from the total number of books
 number_of_pages = ceil(int(total_number_books) / 20)
 print(number_of_pages)
 
+
+
+
+
+
+category = "mystery_3"
+for page in range(2, number_of_pages + 1):
+    url_next_page = f"http://books.toscrape.com/catalogue/category/books/{category}/page-{page}.html"
+    print(url_next_page)
 
