ef9d404aafde33babfba037836bbf8002373c519
alexandre alunno <alexandrealunno@gmail.com>
2021-10-13 10:00:54 +0200

    Changing page + find categories
    -Cleaned Changing page file
    -Created find_categories_url file
    -transfered proto script to find categories url from Changing_page to find_categories

--- Changing_page.py
+++ Changing_page.py
@@ -4,37 +4,23 @@ from math import *
 
 
 #calculate the number of books in a category
-category_url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
+category_url = "http://books.toscrape.com/catalogue/category/books_1/index.html"
 reponse = requests.get(category_url)
 page = reponse.content
 soup = BeautifulSoup(page, "html.parser")
 emplacement = soup.find_all("strong")
 total_number_books = emplacement[1].string
-#print(total_number_books.string)
+print(total_number_books.string)
 
 #calculate the number of pages from the total number of books
 number_of_pages = ceil(int(total_number_books) / 20)
-#print(number_of_pages)
-
-#find the category
-category_name = []
-category_in_html = soup.find("ul", class_='nav nav-list')
-
-#print(category_in_html)
-
-next_category_url = category_in_html.findChildren("li", recursive=True)
-#find children "a"
-category = next_category_url[4].get("href")
-category_name.append(category)
-print(next_category_url[4])
-
-
+print(number_of_pages)
 
 #replace the url by the url of the next page
 
-"""for page in range(2, number_of_pages + 1):
-    url_next_page = f"http://books.toscrape.com/catalogue/category/books/{category}/page-{page}.html"
-    #print(url_next_page)"""
+for page in range(2, number_of_pages + 1):
+    url_next_page = f"http://books.toscrape.com/catalogue/category/books_1/page-{page}.html"
+    print(url_next_page)
 
 
 
